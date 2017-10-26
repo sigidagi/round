9ff6b4e295872186b4844d39e5e336077ff01c87
sigis <sigis@seluxit.com>
2017-10-26 11:59:17 +0200

    added c++11 std::to_string func.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <limits>
+#include <string>
 
 int main(int argc, char* argv[])
 {
-    float min = -16777215.0;
-    float step = 1.0;
-    float realv = 0.0;
+    double min = -16777215.0;
+    double step = 1.0;
+    double realv = 0.0;
 
-    long templ = static_cast<long>(((realv - min) + (double)step/2) / step);
+    long templ = static_cast<long>(((realv - min) + step/2) / step);
     float tempf = templ * step + min;
     
-    std::cout << "Input: " << realv << "\n";
+    std::cout << "Input: " << std::to_string(realv) << "\n";
     std::cout << "Result: " << tempf << "\n";
+    
+
 
     return 0;
 }
